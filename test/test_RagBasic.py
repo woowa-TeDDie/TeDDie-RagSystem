3199c11b10f893b815d43352f555e4e99d71a194
jyt6640 <jyt6640@gmail.com>
2025-11-10 09:33:22 +0900

    test: 테스트를 하기 위한 파일명 변경

--- test/test_RagBasic.py
+++ test/test_RagBasic.py
@@ -1,10 +1,10 @@
 import pytest
 
-from rag_search import WoowacourseRAG
+from rag.RagSearch import WoowacourseRAG
 
 def test_load_documents_returns_list():
     rag = WoowacourseRAG()
-    rag.load._documents()
+    rag.load_documents()
     assert isinstance(rag.documents(), list)
     assert len(rag.documents()) > 0
     
