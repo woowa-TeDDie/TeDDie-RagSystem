9dad35f216c368950e99bbcfc9e1fab404b302c2
jyt6640 <jyt6640@gmail.com>
2025-11-10 10:09:30 +0900

    feat(RagSearch): 임베딩 생성 기능 구현

--- rag/RagSearch.py
+++ rag/RagSearch.py
@@ -1,19 +1,24 @@
 import json
 import os
 
+from sentence_transformers import SentenceTransformer
+
 class WoowacourseRAG:
     def __init__(self, jsonl_path="woowacourse_rag_dataset.jsonl"):
         base_dir = os.path.dirname(os.path.dirname(__file__))
         self.jsonl_path = os.path.join(base_dir, jsonl_path)
         self._documents = []
+        self.model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
     def load_documents(self):
         with open(self.jsonl_path, "r", encoding="utf-8") as f:
             for line in f:
                 record = json.loads(line)
                 self._documents.append(record)
+                
+    def embed_text(self, text: str):
+        return self.model.encode(text)
 
     @property
     def documents(self):
-        """_documents 리스트를 읽기 전용으로 접근할 수 있도록 함."""
         return self._documents
